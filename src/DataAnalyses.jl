ec9eb1dd70f92005261b75c09838b2877ab826f2
Likan Zhan <zhanlikan@hotmail.com>
2021-11-02 13:20:28 +0800

    modified:   Project.toml 	modified:   src/DataAnalyses.jl 	index.jl 	notebooks/ 	src/functionsgeneral.jl 	src/functionsnotebook.jl 	src/functionspage.jl 	src/functionswebsite.jl 	src/notebooklist.jl

--- src/DataAnalyses.jl
+++ src/DataAnalyses.jl
@@ -1,5 +1,21 @@
 module DataAnalyses
 
-# Write your package code here.
+using Pkg, Markdown, PlutoUI
+
+include_dependency(joinpath("..", "Project.toml"))
+include("functionsgeneral.jl")   # project_relative_path()
+include("functionsnotebook.jl")  # list_notebooks()
+include("functionspage.jl")      # footer(), header()
+include("functionswebsite.jl")   # start(), update(), and create_sysimage()
+include("notebooklist.jl")       # notebooklist
+
+const _VERSION = VersionNumber(Pkg.TOML.parsefile(project_relative_path("Project.toml"))["version"])
+@info """\n
+    欢迎来小组的数据分析模块 v$(_VERSION)!
+    请在 REPL 输入下面的命令开始：
+    ```julia
+    julia> DataAnalyses.start()
+    ````
+\n"""
 
 end
