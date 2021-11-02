1da7f82ce8a35b1b6115fa4733ea0a4fd9a7f007
Likan Zhan <zhanlikan@hotmail.com>
2021-11-02 20:33:09 +0800

    modified:   index.jl 	modified:   src/notebooklist.jl

--- src/notebooklist.jl
+++ src/notebooklist.jl
@@ -1,3 +1,4 @@
 const NOTEBOOKS = [
-    ("否定加工",  "Negation/NegationWithoutLanguage.jl"),
+    ("非语言否定",   "Negation/NegationWithoutLanguage.jl"),
+    ("视觉观点采择", "VisualPerspective/VisualPerspective.jl")
     ]
