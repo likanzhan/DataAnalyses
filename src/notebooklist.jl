1b18fd7eb01c833e2db70cbec3031f4f8d957dea
Likan Zhan <zhanlikan@hotmail.com>
2021-11-15 23:45:35 +0800

    new file:   notebooks/AuditoryEmotion/AuditoryEmotion.jl 	new file:   notebooks/AuditoryEmotion/Childdata.xlsx 	modified:   src/notebooklist.jl

--- src/notebooklist.jl
+++ src/notebooklist.jl
@@ -1,4 +1,5 @@
 const NOTEBOOKS = [
+    ("自闭症情绪理解", "AuditoryEmotion/AuditoryEmotion.jl"),
     ("否定加工过程",  "Negation/NegationWithoutLanguage.jl"),
     ("内隐心理理论",  "ImplicitTheoryOfMind/ImplicitTheoryOfMind.jl"),
     ("脑电事件提取",  "Disjunctions/DisjunctionEvents.jl"),
