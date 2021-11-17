f243e4968ce115d2fc0441e69320c6f4b7c11ed5
likanzhan <zhanlikan@hotmail.com>
2021-11-17 04:14:27 +0000

    Deploying to gh-pages from  @ 1409802827236a0c2321bd7aac2361b2bb97dee5 🚀

--- index.jl
+++ index.jl
@@ -24,9 +24,19 @@ DataAnalyses.header(title = "数据分析汇总", author = "")
 
 # ╔═╡ 891c5658-b5e3-4cb8-8a21-9a1c28e122a5
 md"""
+
+为提高科学研究可重复性， 本站汇总了项目组近期开展项目的原始数据及数据分析过程。
+
+"""
+
+# ╔═╡ 82b7a596-1b6c-11ec-1731-5b8c4d67cc2c
+DataAnalyses.list_notebooks(@__FILE__, "notebooks")
+
+# ╔═╡ efe711b2-6a55-4c99-bd0a-b39ce1b2885e
+md"""
 ## Julia 语言
 
-为提高科学研究可重复性，本站汇总了项目组近期开展项目的原始数据及数据分析过程。 本站数据分析的基本工具是 [Julia语言](https://www.julialang.org)。 
+本站数据分析的基本工具是 [Julia语言](https://www.julialang.org)。 
 下面是 Julia 语言的一些常见描述：
 
 - **Walks like Python, runs like C.**
@@ -38,14 +48,11 @@ md"""
 - [The Technical Programming Language of the Future](http://pages.stat.wisc.edu/~bates/JuliaForRProgrammers.pdf) - Douglas Bates
 
 要了解更多 Julia 语言本身的一般知识， 请参看 Julia 语言 的[官方文档](https://docs.julialang.org)。
-
 """
-
-# ╔═╡ 82b7a596-1b6c-11ec-1731-5b8c4d67cc2c
-DataAnalyses.list_notebooks(@__FILE__, "notebooks")
 
 # ╔═╡ Cell order:
 # ╟─47554e79-497d-411d-ab2f-182cc915af48
 # ╟─a888c516-dba1-4cda-9ada-2e516fc106e9
 # ╟─891c5658-b5e3-4cb8-8a21-9a1c28e122a5
 # ╟─82b7a596-1b6c-11ec-1731-5b8c4d67cc2c
+# ╟─efe711b2-6a55-4c99-bd0a-b39ce1b2885e
