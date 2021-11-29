14c593bf1ca206883b6830d1405c2dc6486fcdb1
likanzhan <zhanlikan@hotmail.com>
2021-11-29 16:33:52 +0000

    Deploying to gh-pages from  @ d341ca41a2e9d49e58310c90e9d127f08a084ce8 🚀

--- src/notebooklist.jl
+++ src/notebooklist.jl
@@ -3,5 +3,6 @@ const NOTEBOOKS = [
     ("否定加工过程",  "Negation/NegationWithoutLanguage.jl"),
     ("内隐心理理论",  "ImplicitTheoryOfMind/ImplicitTheoryOfMind.jl"),
     ("脑电事件提取",  "Disjunctions/DisjunctionEvents.jl"),
+    ("脑电事件转化",  "Disjunctions/RefactorTriggers.jl"),
     ("视觉观点采择",  "VisualPerspective/VisualPerspective.jl")
     ]
