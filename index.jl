b0122e3779e53ae9b9b18d2799406ad8a932c2a4
Likan Zhan <zhanlikan@hotmail.com>
2022-04-08 09:59:43 +0800

    update regret

--- index.jl
+++ index.jl
@@ -1,5 +1,5 @@
 ### A Pluto.jl notebook ###
-# v0.17.3
+# v0.19.0
 
 using Markdown
 using InteractiveUtils
@@ -24,9 +24,7 @@ DataAnalyses.header(title = "数据分析汇总", author = "")
 
 # ╔═╡ 891c5658-b5e3-4cb8-8a21-9a1c28e122a5
 md"""
-
 为提高科学研究可重复性， 本站汇总了项目组近期开展项目的原始数据及数据分析过程。
-
 """
 
 # ╔═╡ 82b7a596-1b6c-11ec-1731-5b8c4d67cc2c
