aea65ff2d1ece7b7de58187f8df69a832c998c32
Likan Zhan <zhanlikan@hotmail.com>
2022-04-11 19:59:10 +0800

    Simply structure

--- src/DataAnalyses.jl
+++ src/DataAnalyses.jl
@@ -1,21 +1,15 @@
 module DataAnalyses
 
-using Pkg, Markdown
-
-include_dependency(joinpath("..", "Project.toml"))
-include("functionsgeneral.jl")   # project_relative_path()
-include("functionsnotebook.jl")  # list_notebooks()
-include("functionspage.jl")      # footer(), header()
-include("functionswebsite.jl")   # start(), update(), and create_sysimage()
-include("notebooklist.jl")       # notebooklist
-
-const _VERSION = VersionNumber(Pkg.TOML.parsefile(project_relative_path("Project.toml"))["version"])
-@info """\n
-    欢迎来小组的数据分析模块 v$(_VERSION)!
-    请在 REPL 输入下面的命令开始：
-    ```julia
-    julia> DataAnalyses.start()
-    ````
-\n"""
+function start()
+    root =  normpath(joinpath(Base.find_package("DataAnalyses"), "..", ".."))
+    indexpath = joinpath(root, "index.jl")
+    exe = joinpath(Sys.BINDIR, "julia")
+    script = :(using Pkg;
+                Pkg.activate($root);
+                using Pluto;
+                Pluto.run(notebook = $indexpath)
+    )
+    run(`$exe -e $script`)
+end
 
 end
